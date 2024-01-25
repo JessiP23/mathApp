a416a2a6c127c4ebb7bc07874378b7bb67965be6
Jessi Pavia <jessi316866@gmail.com>
2024-01-25 13:33:57 -0500

    eighteen commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
